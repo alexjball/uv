0f2242aee7f8f1309adc1bd9281f3c445ea879d8
Charlie Marsh <charlie.r.marsh@gmail.com>
2025-03-01 20:12:37 -0500

    Avoid silently dropping errors in directory enumeration

--- crates/uv-distribution/src/source/built_wheel_metadata.rs
+++ crates/uv-distribution/src/source/built_wheel_metadata.rs
@@ -29,7 +29,7 @@ pub(crate) struct BuiltWheelMetadata {
 impl BuiltWheelMetadata {
     /// Find a compatible wheel in the cache.
     pub(crate) fn find_in_cache(tags: &Tags, cache_shard: &CacheShard) -> Option<Self> {
-        for file in files(cache_shard) {
+        for file in files(cache_shard).ok()? {
             if let Some(metadata) = Self::from_path(file, cache_shard) {
                 // Validate that the wheel is compatible with the target platform.
                 if metadata.filename.is_compatible(tags) {
